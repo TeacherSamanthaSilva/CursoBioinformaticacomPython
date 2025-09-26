91f83943b58a9c8b72520509137276e683665ce1
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-09-26 12:49:45 -0300

    Create Exercicio5.py
    nesse commit fiz o exercicio 5 Escreva um programa que compare se duas sequências digitadas pelo usuário são iguais.

---
+++ Exercicio5.py
@@ -0,0 +1,11 @@
+# Programa para comparar duas sequências
+
+# Entrada de dados
+seq1 = input("Digite a primeira sequência: ")
+seq2 = input("Digite a segunda sequência: ")
+
+# Comparação
+if seq1 == seq2:
+    print("As sequências são iguais!")
+else:
+    print("As sequências são diferentes!")
